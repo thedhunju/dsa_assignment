4aec5855f9c87ff6442fc8d3eeb1d32a57acea25
thedhunju <pujandhunju@gmail.com>
2026-02-10 18:51:23 +0545

    feat: implement various DSA programs including graph traversals, heaps, Dijkstra's, sorting algorithms, and bracket balancing.

--- Program/prog_6.c
+++ Program/prog_6.c
@@ -1,74 +1,74 @@
 #include <stdio.h>
 
 void swp(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
 void minH(int arr[], int size, int index) {
-    int smallest = index;
-    int left = 2*index + 1;
-    int right = 2*index + 2;
-    if (left < size && arr[left] < arr[smallest])
-        smallest = left;
-    if (right < size && arr[right] < arr[smallest])
-        smallest = right;
-    if (smallest != index) {
-        swp(&arr[index], &arr[smallest]);
-        minH(arr, size, smallest);
-    }
+  int smallest = index;
+  int left = 2*index + 1;
+  int right = 2*index + 2;
+  if (left < size && arr[left] < arr[smallest])
+    smallest = left;
+  if (right < size && arr[right] < arr[smallest])
+    smallest = right;
+  if (smallest != index) {
+    swp(&arr[index], &arr[smallest]);
+    minH(arr, size, smallest);
+  }
 }
 
 void maxH(int arr[], int size, int index) {
-    int largest = index;
-    int left = 2*index + 1;
-    int right = 2*index + 2;
-    if (left < size && arr[left] > arr[largest])
-        largest = left;
-    if (right < size && arr[right] > arr[largest])
-        largest = right;
-    if (largest != index) {
-        swp(&arr[index], &arr[largest]);
-        maxH(arr, size, largest);
-    }
+  int largest = index;
+  int left = 2*index + 1;
+  int right = 2*index + 2;
+  if (left < size && arr[left] > arr[largest])
+    largest = left;
+  if (right < size && arr[right] > arr[largest])
+    largest = right;
+  if (largest != index) {
+    swp(&arr[index], &arr[largest]);
+    maxH(arr, size, largest);
+  }
 }
 
 void buildMin(int arr[], int size) {
-    int i;
-    for (i = size/2 - 1; i >= 0; i--)
-        minH(arr, size, i);
+  int i;
+  for (i = size/2 - 1; i >= 0; i--)
+    minH(arr, size, i);
 }
 
 void buildMax(int arr[], int size) {
-    int i;
-    for (i = size/2 - 1; i >= 0; i--)
-        maxH(arr, size, i);
+  int i;
+  for (i = size/2 - 1; i >= 0; i--)
+    maxH(arr, size, i);
 }
 
 void show(int arr[], int size) {
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+  int i;
+  for (i = 0; i < size; i++)
+    printf("%d ", arr[i]);
+  printf("\n");
 }
 
 int main() {
-    int size, i;
-    printf("Enter size: ");
-    scanf("%d", &size);
-    int arr[size], minHeap[size], maxHeap[size];
-    printf("Enter elements:\n");
-    for (i = 0; i < size; i++) {
-        scanf("%d", &arr[i]);
-        minHeap[i] = arr[i];
-        maxHeap[i] = arr[i];
-    }
-    buildMin(minHeap, size);
-    buildMax(maxHeap, size);
-    printf("Min Heap: ");
-    show(minHeap, size);
-    printf("Max Heap: ");
-    show(maxHeap, size);
-    return 0;
+  int size, i;
+  printf("Enter size: ");
+  scanf("%d", &size);
+  int arr[size], minHeap[size], maxHeap[size];
+  printf("Enter elements:\n");
+  for (i = 0; i < size; i++) {
+    scanf("%d", &arr[i]);
+    minHeap[i] = arr[i];
+    maxHeap[i] = arr[i];
+  }
+  buildMin(minHeap, size);
+  buildMax(maxHeap, size);
+  printf("Min Heap: ");
+  show(minHeap, size);
+  printf("Max Heap: ");
+  show(maxHeap, size);
+  return 0;
 }
--- Program/prog_7.c
+++ Program/prog_7.c
@@ -3,69 +3,69 @@
 #define INF 9999
 
 int getMin(int d[], int v[], int s) {
-    int m = INF, idx = -1;
-    int i;
-    for (i = 0; i < s; i++) {
-        if (v[i] == 0 && d[i] < m) {
-            m = d[i];
-            idx = i;
-        }
+  int m = INF, idx = -1;
+  int i;
+  for (i = 0; i < s; i++) {
+    if (v[i] == 0 && d[i] < m) {
+      m = d[i];
+      idx = i;
     }
-    return idx;
+  }
+  return idx;
 }
 
 void path(int p[], int j) {
-    if (p[j] == -1) {
-        printf("%d ", j);
-        return;
-    }
-    path(p, p[j]);
+  if (p[j] == -1) {
     printf("%d ", j);
+    return;
+  }
+  path(p, p[j]);
+  printf("%d ", j);
 }
 
 void solve(int g[N][N], int src) {
-    int d[N], v[N], p[N];
-    int i, c, u, w;
+  int d[N], v[N], p[N];
+  int i, c, u, w;
+  
+  for (i = 0; i < N; i++) {
+    d[i] = INF;
+    v[i] = 0;
+    p[i] = -1;
+  }
+  
+  d[src] = 0;
+  
+  for (c = 0; c < N - 1; c++) {
+    u = getMin(d, v, N);
+    v[u] = 1;
     
-    for (i = 0; i < N; i++) {
-        d[i] = INF;
-        v[i] = 0;
-        p[i] = -1;
+    for (w = 0; w < N; w++) {
+      if (v[w] == 0 && g[u][w] && d[u] + g[u][w] < d[w]) {
+        p[w] = u;
+        d[w] = d[u] + g[u][w];
+      }
     }
-    
-    d[src] = 0;
-    
-    for (c = 0; c < N - 1; c++) {
-        u = getMin(d, v, N);
-        v[u] = 1;
-        
-        for (w = 0; w < N; w++) {
-            if (v[w] == 0 && g[u][w] && d[u] + g[u][w] < d[w]) {
-                p[w] = u;
-                d[w] = d[u] + g[u][w];
-            }
-        }
-    }
-    
-    printf("Vertex\tDist\tPath\n");
-    for (i = 0; i < N; i++) {
-        printf("%d\t%d\t", i, d[i]);
-        path(p, i);
-        printf("\n");
-    }
+  }
+  
+  printf("Vertex\tDist\tPath\n");
+  for (i = 0; i < N; i++) {
+    printf("%d\t%d\t", i, d[i]);
+    path(p, i);
+    printf("\n");
+  }
 }
 
 int main() {
-    int g[N][N] = {
-        {0, 4, 0, 0, 0, 0},
-        {4, 0, 8, 0, 0, 0},
-        {0, 8, 0, 7, 0, 4},
-        {0, 0, 7, 0, 9, 14},
-        {0, 0, 0, 9, 0, 10},
-        {0, 0, 4, 14, 10, 0}
-    };
-    
-    solve(g, 0);
-    
-    return 0;
+  int g[N][N] = {
+    {0, 4, 0, 0, 0, 0},
+    {4, 0, 8, 0, 0, 0},
+    {0, 8, 0, 7, 0, 4},
+    {0, 0, 7, 0, 9, 14},
+    {0, 0, 0, 9, 0, 10},
+    {0, 0, 4, 14, 10, 0}
+  };
+  
+  solve(g, 0);
+  
+  return 0;
 }
